7074a595c51f2379fcb5e984808276acfa15e779
Tamer Halimoglu <tamer.halimoglu@pulseenergy.com>
2014-11-24 16:48:34 -0800

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
 
-//locations
-var coffeeSource = 'components/coffee/tagline.coffee';
+
+
+//Source locations
+var coffeeSource = ['components/coffee/tagline.coffee'];
+var jsSources =  [
+	'components/scripts/pixgrid.js',
+	'components/scripts/rclick.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js',
+];
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSource).
 		pipe(coffee({bare:true}).on('error', gutil.log)).
-			pipe(gulp.dest('components/scripts'))
+			pipe(gulp.dest('components/scripts'));
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSources).
+		pipe(concat('scripts.js')).
+			pipe(browserify()).
+			pipe(gulp.dest('builds/development/js'));
 })
